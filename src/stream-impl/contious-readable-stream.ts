e1f1c0bc5a4c7e7367e616003661b1a657f4cfe3
Damian Wilkołek <dwilkolek@protonmail.com>
2019-03-29 04:40:37 +0100

    faster writable ;)

--- src/stream-impl/contious-readable-stream.ts
+++ src/stream-impl/contious-readable-stream.ts
@@ -4,7 +4,11 @@ const fs = require('fs');
 var es = require('event-stream');
 export class ContinoiusReadableStream extends Readable {
 
+    constructor() {
+        super({highWaterMark:1})
+    }
     _read(size: number) {
+        
         this.push(JSON.stringify({name:"damian", age:Math.round(Math.random()*100)}));
     }
 
--- src/stream-impl/writable-stream-with-store-offset.ts
+++ src/stream-impl/writable-stream-with-store-offset.ts
@@ -4,18 +4,18 @@ import { JobContext } from "../types/job-context";
 export class WritableStreamWithStoreOffset extends Writable {
     offset = 0;
     constructor(private context: JobContext) {
-        super();
+        super({highWaterMark: 1});
         if (context.currentOffset) {
             this.offset = context.currentOffset;
         }
     }
     _write(chunk: any, encoding: string, callback: (error?: Error | null) => void) {
+        this.cork();
         this.offset++;
         console.log('storing offset ', this.offset)
         this.context.storeOffset(this.offset, (result) => {
-            setTimeout(() => {
+                this.uncork();
                 callback()
-            }, 2000);
         });
 
     }
